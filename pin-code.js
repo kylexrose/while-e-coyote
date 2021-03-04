62e2ecbdf874fb04f26de8bb80575660286d9358
kylexrose@gmail.com <kylexrose@gmail.com>
2021-03-04 13:12:11 -0500

    completed assignment and checked logs

--- pin-code.js
+++ pin-code.js
@@ -1,3 +1,13 @@
 const getInput = require('./get-input.js');
-
+const correctPin = "1234";
+let counter = 1;
 // Your code here!
+while(getInput(counter) !== undefined){
+    if (getInput(counter) === correctPin){
+        console.log("That is correct. You took " + counter + " guess(es).")
+        break;
+    }else(counter++)
+}
+if (getInput(counter) !== correctPin){
+    console.log("None of the entered pin codes are correct.")
+}
--- sum-args.js
+++ sum-args.js
@@ -1,3 +1,12 @@
 const getInput = require('./get-input.js');
 
+let i = 1;
+let total = 0;
+
+while(getInput(i) !== undefined){
+    let current = Number(getInput(i));
+    total += current;
+    i++;
+}
+console.log(total);
 // Your code here!
